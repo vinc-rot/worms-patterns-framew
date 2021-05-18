cccfc4ca53df0f9945b52ed6840346221e2d8e7d
vinc-rot <vrhack1@gmail.com>
2021-05-18 15:59:04 +0200

    SetOnAction fehlt noch.

---
+++ wormsfx/src/module-info.java
@@ -0,0 +1,10 @@
+module javafx {
+    requires  javafx.fxml;
+    requires  javafx.controls;
+    requires  javafx.graphics;
+    requires  javafx.media;
+    requires  javafx.web;
+    requires  javafx.swt;
+
+    opens sample;
+}
